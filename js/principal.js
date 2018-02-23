7e99dbc16ec456962b232371fc925f7edccab14a
AlejandraCP <alejandracp710@gmail.com>
2018-02-23 14:57:00 -0500

    Add datadash

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-    $('.sidenav').sidenav();
+$(document).ready(function() {
+  $('.sidenav').sidenav();
 });
 
 var $spendMonth = $('.spend-month-p');
@@ -8,6 +8,7 @@ var $typeCount = $('.type-count');
 var $residue = $('.residue');
 var $typeCurrent = $('.type-curr');
 var $boxNone = $('.box-none');
+var $comentary = $('#comentary');
 var url = 'https://alejandracp.github.io/BBVA-Json/data.json';
 
 var proxy = 'https://cors-anywhere.herokuapp.com/';
@@ -22,7 +23,7 @@ $.ajax({
   }
 });
 
-// Estadod de cuenta
+// Estado de cuenta
 function getStatus(data) {
   if (data.CODIGO_ESTADO === 200) {
     dataAccounts(data);
@@ -32,18 +33,17 @@ function getStatus(data) {
 var i = 0;
 var consum = 0;
 var j = 0;
+var num = 0;
 // Muestra número de cuenta, saldo y suma de saldos.
 function dataAccounts(data) {
   var cantAcounts = data.DATA.LISTA_CTA;
   var creditCount = data.DATA.LISTA_TARJ;
-
   $.each(cantAcounts, function(index, value) {
-    var num = parseFloat(value.SALDO.replace(',', ''));
+    num = parseFloat(value.SALDO.replace(',', ''));
     $numberCount.append(`<p class="color-blue-text bold">${value.NCUENTA}</p>`);
     $numberCount.append(`<p class="type-count display-block">${value.TIPO_CTA}</p>`);
     $residue.append(`<p class="color-blue-text bold">${value.SALDO}</p>`);
     $residue.append(`<p class="type-count size-type">NUEVOS ${value.MONEDA}</p>`);
-    // Suma total de cuentas saldo
     i += num;
   });
   $.each(creditCount, function(index, value) {
@@ -54,15 +54,30 @@ function dataAccounts(data) {
     var creditAble = parseFloat(value.CREDITODISPONIBLE.replace(',', ''));
     var creditLimit = parseFloat(value.LIMITECREDITO.replace(',', ''));
     consum = creditLimit - creditAble;
-    // numbersAcounts.append(`<p>${creditAble}</p>`);
   });
-//   income.append(`Su ingreso total es ${i.toFixed(2)}`);
-  $spendMonth.append(`${consum.toFixed(2)}`);
+
+  function comentaryConti(num) {
+    if (num > 5000) {
+      $comentary.html(
+        '<p>Sabías que con el gasto de este mes podrías ahorrar para empezar a pagar tu futuro departamento</p>'
+      );
+    } else if (num > 2000) {
+      $comentary.html(
+        '<p>Sabías que con el gasto de este mes podrías hacer un viaje a Cusco</p>'
+      );
+    } else if (num < 500) {
+      $comentary.html(
+        '<p>Sabías que con el gasto de este mes podrías pasar un fin de semanacon tu familia fuera de Lima</p>'
+      );
+    }
+  }
+  $spendMonth.append(i.toFixed(2));
+  comentaryConti(consum);
 }
 
 google.charts.load('current', { packages: ['corechart'] });
 google.charts.setOnLoadCallback(drawChart);
-// google.charts.setOnLoadCallback(drawChart2);
+google.charts.setOnLoadCallback(drawChart2);
 function drawChart() {
   var data = google.visualization.arrayToDataTable([
     ['Task', 'dg'],
@@ -71,8 +86,7 @@ function drawChart() {
   ]);
   var options = {
     title: 'Consumo vs Ingreso',
-    // pieHole: 0.4,
-    colors: ['#02428C', '#e7101e']
+    colors: ['#02428C', '#61C2F0']
   };
 
   var chart = new google.visualization.PieChart(
@@ -112,30 +126,8 @@ function expensesMonths(data) {
   console.log(setiembre);
 }
 
-// Google charts donuts
-google.charts.load('current', { packages: ['corechart'] });
-google.charts.setOnLoadCallback(drawChart);
-google.charts.setOnLoadCallback(drawChart2);
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-    ['Task', 'dg'],
-    ['Ingreso', Number(i.toFixed(2))],
-    ['Consumo', Number(consum.toFixed(2))]
-  ]);
-  var options = {
-    title: 'My Daily Activities',
-    pieHole: 0.4,
-    colors: ['#02428C', '#e7101e']
-  };
-
-  var chart = new google.visualization.PieChart(
-    document.getElementById('donut1')
-  );
-  chart.draw(data, options);
-}
 
 // google charts year
-
 function drawChart2() {
   var data = new google.visualization.arrayToDataTable([
     ['Mes', 'Consumo'],
@@ -155,7 +147,8 @@ function drawChart2() {
 
   var options = {
     title: 'Gasto mensual 2017',
-    hAxis: { title: 'Year', titleTextStyle: { color: '#02428C' } },
+    hAxis: { title: 'Year',
+      titleTextStyle: { color: '#02428C' } },
     vAxis: { minValue: 0 }
   };
 
